773b3873c4e3ad5d4fad501df6af27ac48794f41
aniket <aniket.umbc@gmail.com>
2024-04-24 12:42:17 +0530

    special case for hoisting

--- javascript-concept/session-2-hoisting.js
+++ javascript-concept/session-2-hoisting.js
@@ -7,3 +7,25 @@ var studentName = 'Bunny';
 function greet() {
   console.log('Welcome to greet');
 }
+
+/**
+ *  Case: 1 variable has value assingn then variable has higher piority
+ */
+var studentRoll = 1000;
+
+function studentRoll() {
+  console.log('Calling function variable');
+}
+
+console.log(studentRoll);
+
+/**
+ *  Case: 2 variable has no value assingn then function has higher piority
+ */
+var studentNewRoll;
+
+function studentNewRoll() {
+  console.log('Calling function variable');
+}
+
+console.log(studentNewRoll);
