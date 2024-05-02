bf01c49f39d1cbb767cea661c42c2afc29e2b9c8
aniket <aniket.umbc@gmail.com>
2024-05-02 18:00:12 +0530

    Promise with callback hell

--- javascript-concept/asyncCode.js
+++ javascript-concept/asyncCode.js
@@ -1,3 +1,7 @@
+const btn = document.querySelector('button');
+
+// btn.addEventListener('click', callingBtn);
+
 setTimeout(() => {
   console.log('timer complete');
 }, 2000);
@@ -19,3 +23,49 @@ function greetMeet(age) {
 }
 
 greet('Bunny', 10, greetMeet);
+
+function callingBtn() {
+  console.log('click me');
+}
+
+/**
+ *
+ *  Promise setup by step manage callback function is hard to understand and manage.
+ *  So we use callback
+ *  1. it java sctipt object with certain operation.
+ *  2. promise eventully finished or settled
+ * 3. then method exectute when promise settled
+ * 4. to avoid call back hell
+ *  there 3 state
+ * 1. pending  ( it is initial state Promise not produce result yet)
+ * 2.Fatullfiled  once is compelte state  promise resloved
+ * 3. Rejected state ( data failed to get) promise rejected
+ * onces promise relsove then function gets execute
+ */
+
+const myPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Yes it's woking ");
+  }, 2000);
+});
+
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then((res) => res.json())
+  .then((data) => console.log(data));
+
+function callPromise() {
+  myPromise
+    .then((data) => {
+      console.log(data);
+    })
+    .then(() => {
+      console.log('second then method');
+    })
+    .then(() => {
+      setTimeout(() => {
+        console.log('this is next thrid then');
+      }, 5000);
+    });
+}
+
+btn.addEventListener('click', callPromise);
