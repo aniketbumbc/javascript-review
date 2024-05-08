d0b9271aadaaa648ee2759acafd2ff8b1a24b0e2
aniket <aniket.umbc@gmail.com>
2024-05-08 05:32:45 +0530

    fibonacci series

--- javascript-concept/algoritham/seesion-1.js
+++ javascript-concept/algoritham/seesion-1.js
@@ -14,4 +14,38 @@ function calculateSum(arr) {
   //console.log(result);
 }
 
-console.log(calculateSum([1, 2, 4, 5, 6, 100, 22]));
+// console.log(calculateSum([1, 2, 4, 5, 6, 100, 22]));
+
+/**
+ *  Maths algoritham fibonacci series
+ */
+
+function fibo(num) {
+  let result = 0;
+
+  for (let i = 1; i <= num; i++) {
+    console.log(i, 'result', result);
+    result = result + i;
+  }
+
+  return result;
+}
+
+// console.log(fibo(10));
+
+function fib(num) {
+  const nums = [1, 1];
+
+  for (let i = 2; i <= num; i++) {
+    console.log('nums');
+    nums.push(nums[i - 2] + nums[i - 1]);
+  }
+
+  return nums[num - 1];
+}
+
+console.log(fib(10));
+
+/**
+ *  Prime number
+ */
