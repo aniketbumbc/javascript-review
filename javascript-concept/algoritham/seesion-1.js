91d0b56751a5c14b68f8deb44916b2f4c8c53d50
aniket <aniket.umbc@gmail.com>
2024-05-09 11:01:56 +0530

    Practice for factorial and Power of two

--- javascript-concept/algoritham/seesion-1.js
+++ javascript-concept/algoritham/seesion-1.js
@@ -44,8 +44,87 @@ function fib(num) {
   return nums[num - 1];
 }
 
-console.log(fib(10));
+// console.log(fib(10));
 
 /**
- *  Prime number
+ *  Prime number or not
  */
+
+function isPrimeNumber(num) {
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(isPrimeNumber(9));
+
+/**
+ *  Find min number into array
+ */
+
+function findMinNum(arr) {
+  const minNum = Math.min(...arr);
+
+  return minNum;
+}
+
+console.log(findMinNum([4, 6, 3, 7, 10, 9, 1]));
+
+/**
+ *  Power of two
+ */
+
+function powerOfTwo(num) {
+  if (num < 1) {
+    return false;
+  }
+
+  let dividedNum = num;
+
+  while (dividedNum !== 1) {
+    if (dividedNum % 2 !== 0) {
+      return false;
+    }
+
+    dividedNum = dividedNum / 2;
+  }
+  return true;
+}
+
+console.log(powerOfTwo(16));
+console.log(powerOfTwo(15));
+
+/**
+ *  Factorial
+ */
+
+function fact(num) {
+  let result = 1;
+
+  for (let i = num; i >= 1; i--) {
+    console.log(i);
+    result = result * i;
+    console.log(result);
+  }
+
+  console.log(result);
+}
+
+fact(4);
+
+/**
+ *  Factorial with recursion
+ */
+
+function recursionFacto(num) {
+  if (num === 1) {
+    return 1;
+  }
+
+  return recursionFacto(num - 1) * num;
+}
+
+console.log(recursionFacto(5));
