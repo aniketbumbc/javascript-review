2e9f8d4b66a0740938a249467c8d13519b03327a
aniket <aniket.umbc@gmail.com>
2024-05-15 05:47:40 +0530

    Bubble sort

---
+++ javascript-concept/algoritham/sorting /sorting.js
@@ -0,0 +1,19 @@
+/**
+ *  Bubble sort
+ */
+
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+
+  return arr;
+}
+
+console.log(bubbleSort([6, 3, 2, 7, 1]));
