01124c56371d9cc266454bbb522a1d703265a0dd
aniket <aniket.umbc@gmail.com>
2024-05-21 06:50:38 +0530

    coins problem slove

--- javascript-concept/algoritham/practice problems/session 21.05.js
+++ javascript-concept/algoritham/practice problems/session 21.05.js
@@ -1,3 +1,9 @@
+/** One item at time try to make best decision at time.
+ *
+ * @param {*} ele
+ * @param {*} cap
+ */
+
 function knapsackGreedy(ele, cap) {
   const sack = { items: [], value: 0, weight: 0 };
   let remainCapacity = cap;
@@ -24,4 +30,39 @@ const maxCap = 8;
 
 const sack = knapsackGreedy(items, maxCap);
 
-console.log(sack);
+// console.log(sack);
+
+/**
+ *
+ *  coin problem
+ */
+
+const coins = [100, 50, 20, 10, 5, 2, 1];
+const target = 50;
+
+function calculateValue(coins, value) {
+  const calculateCoins = {};
+  let remainingTarget = value;
+  let calculateTotalCoins = 0;
+
+  for (let ele of coins) {
+    calculateCoins[ele] = 0;
+    while (ele <= remainingTarget) {
+      calculateCoins[ele]
+        ? (calculateCoins[ele] += 1)
+        : (calculateCoins[ele] = 1);
+
+      remainingTarget -= ele;
+    }
+  }
+
+  for (key in calculateCoins) {
+    calculateTotalCoins += calculateCoins[key];
+  }
+
+  return { calculateCoins, calculateTotalCoins };
+}
+
+const result = calculateValue(coins, target);
+
+console.log(result);
