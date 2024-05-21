98b388764ab6eb358637e95f3d7f47a60e7b8aa3
aniket <aniket.umbc@gmail.com>
2024-05-21 07:21:23 +0530

    Brute force implemented

--- javascript-concept/algoritham/practice problems/session 21.05.js
+++ javascript-concept/algoritham/practice problems/session 21.05.js
@@ -37,10 +37,11 @@ const sack = knapsackGreedy(items, maxCap);
  *  coin problem
  */
 
-const coins = [100, 50, 20, 10, 5, 2, 1];
-const target = 50;
+const coins = [8, 6, 5, 1];
+const target = 63;
 
 function calculateValue(coins, value) {
+  //
   const calculateCoins = {};
   let remainingTarget = value;
   let calculateTotalCoins = 0;
@@ -63,6 +64,29 @@ function calculateValue(coins, value) {
   return { calculateCoins, calculateTotalCoins };
 }
 
-const result = calculateValue(coins, target);
+// const result = calculateValue(coins, target);
 
-console.log(result);
+// console.log(result);
+
+/**
+ *  Another approach  all possible solution
+ */
+
+function computeBruteForce(coins, target) {
+  const results = [];
+  for (let i = 0; i < coins.length; i++) {
+    results.push(calculateValue(coins.slice(i), target));
+  }
+
+  let smallAmountCoins = Number.MAX_SAFE_INTEGER;
+  let finalResult;
+  for (let ele of results) {
+    if (ele.calculateTotalCoins < smallAmountCoins) {
+      smallAmountCoins = ele.calculateTotalCoins;
+      finalResult = ele;
+    }
+  }
+  return finalResult;
+}
+
+console.log(computeBruteForce(coins, target));
