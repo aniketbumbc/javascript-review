cf3536713b3abe164f63b5b193d8e46c940527a8
aniket <aniket.umbc@gmail.com>
2024-05-22 10:36:47 +0530

    sets and map practice

---
+++ javascript-concept/algoritham/practice problems/session 22.05.js
@@ -0,0 +1,27 @@
+const ids = new Set();
+
+ids.add('Mike');
+ids.add('Bunny');
+ids.add(1);
+ids.add(2);
+ids.add(1);
+
+console.log(ids);
+console.log(ids.has(2));
+
+ids.delete('Bunny');
+console.log(ids);
+
+const setArray = [...ids];
+
+console.log(setArray);
+
+const resultData = new Map();
+
+resultData.set('avg', 12.2);
+
+resultData.set('lastResult', null);
+
+resultData.set('setsData', ids);
+
+console.log(resultData);
