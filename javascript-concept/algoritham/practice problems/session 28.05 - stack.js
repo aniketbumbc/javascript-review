2fe40ef8309921e8a2c50146240b11e6410d4787
aniket <aniket.umbc@gmail.com>
2024-05-29 06:08:32 +0530

    stack with link-list

--- javascript-concept/algoritham/practice problems/session 28.05 - stack.js
+++ javascript-concept/algoritham/practice problems/session 28.05 - stack.js
@@ -25,12 +25,86 @@ class Stack {
   }
 }
 
-const s1 = new Stack();
+// const s1 = new Stack();
 
-s1.push('A1');
-s1.push('A2');
-s1.push('A3');
-console.log(s1.displayToArray());
-console.log(s1.pop());
-console.log(s1.isEmpty());
-console.log(s1.displayToArray());
+// s1.push('A1');
+// s1.push('A2');
+// s1.push('A3');
+// console.log(s1.displayToArray());
+// console.log(s1.pop());
+// console.log(s1.isEmpty());
+// console.log(s1.displayToArray());
+
+/**
+ *  Stack using link-list
+ */
+
+class StackLinkList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  push(value) {
+    /*
+    **
+    Add the item first to the list
+    */
+    const newNode = { value: value, next: this.head };
+    this.head = newNode;
+    if (!this.tail) {
+      this.tail = newNode;
+    }
+  }
+
+  pop() {
+    if (!this.head) {
+      return null;
+    }
+    /*
+    **
+    delete the item first to the list
+    */
+
+    const deleteItem = this.head;
+
+    if (this.head.next) {
+      this.head = this.head.next;
+    } else {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return deleteItem;
+  }
+
+  isEmpty() {
+    return !this.head;
+  }
+
+  toDisplayList() {
+    const element = [];
+    let currentNode = this.head;
+    while (currentNode) {
+      element.push(currentNode);
+      currentNode = currentNode.next;
+    }
+    return element;
+  }
+}
+
+const stacklist = new StackLinkList();
+
+stacklist.push('Test');
+stacklist.push('Test-1');
+stacklist.push('Test-2');
+stacklist.push('Test-3');
+
+console.log(stacklist.toDisplayList());
+
+console.log(stacklist.pop());
+console.log(stacklist.pop());
+console.log(stacklist.pop());
+console.log(stacklist.pop());
+console.log(stacklist.toDisplayList());
+console.log(stacklist.isEmpty());
