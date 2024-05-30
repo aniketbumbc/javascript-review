1e0b68762d62e2501b60b44a68909b14eca671fa
aniket <aniket.umbc@gmail.com>
2024-05-30 11:40:10 +0530

    to show hash on log write method

--- javascript-concept/algoritham/practice problems/session 30.05 - hash table.js
+++ javascript-concept/algoritham/practice problems/session 30.05 - hash table.js
@@ -21,3 +21,62 @@ function findFirstRepeatChar(str) {
 }
 
 console.log(findFirstRepeatChar(word));
+
+/**
+ *  Hash table
+ *  Index and value
+ */
+
+class HashTable {
+  constructor() {
+    this.size = 1000;
+    this.buckets = Array(100).fill(null);
+  }
+
+  /**
+   *  return index from array as number
+   * @param { } key
+   */
+
+  hash(key) {
+    let hash = 0;
+    for (const char of key) {
+      hash += char.charCodeAt(0);
+    }
+
+    return hash % this.size;
+  }
+
+  /**
+   *  set key and value
+   */
+
+  set(key, value) {
+    const keyHash = this.hash(key);
+    this.buckets[keyHash] = value;
+
+    console.log(this.buckets);
+  }
+
+  get(key) {
+    const keyHash = this.hash(key);
+    console.log(keyHash);
+    return this.buckets[keyHash];
+  }
+
+  showInfo() {
+    for (const key in this.buckets) {
+      if (this.buckets[key] !== null) {
+        console.log(key, this.buckets[key]);
+      }
+    }
+  }
+}
+
+const h1 = new HashTable();
+
+h1.set('aaa', 1);
+h1.set('b', 2);
+h1.set('c', 3);
+
+console.log(h1.get('c'));
