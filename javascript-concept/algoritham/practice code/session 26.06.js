f2495b78ac3a8ab697643d0e2142af8f458b3c24
aniket <aniket.umbc@gmail.com>
2024-06-26 13:15:14 +0530

    proxy in js

--- javascript-concept/algoritham/practice code/session 26.06.js
+++ javascript-concept/algoritham/practice code/session 26.06.js
@@ -71,3 +71,27 @@ function findMaxChar(str) {
 }
 
 findMaxChar('apple banale');
+
+/**
+ * Then Proxy It helps you create another object on behalf of the original object.
+ * This new proxy object will act as a mediator between the real world and the original object.
+ * In this way, we will have more control over the interaction with the original object.
+ * https://www.javascripttutorial.net/javascript-proxy/
+ */
+
+const books = {
+  Deep: 'Cal Newport',
+  Atomic: 'James Clear',
+};
+
+const handler = {
+  get(target, property) {
+    console.log(`Property ${property} has been read.`);
+    return target[property];
+  },
+};
+
+const proxyUser = new Proxy(books, handler);
+
+console.log(proxyUser.Deep);
+console.log(proxyUser.Atomic);
